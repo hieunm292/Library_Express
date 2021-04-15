66a151fafef00a7f81fb71b0d06a8b08c7aabb09
= <=>
2021-04-16 04:51:27 +0700

    product

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser')
 
 const userRoute=require('./routes/user.route')
 const authMiddleware=require('./middlewares/auth.middleware')
+const productRoute=require('./routes/product.route')
 const auth=require('./routes/auth.route')
 const port = 3000
 
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
 
 app.use('/users',authMiddleware.requireAuth , userRoute)
 app.use('/auth',auth)
+app.use('/products', productRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
