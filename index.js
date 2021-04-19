b3d5f83b49e3e847b53e5b3c4b85126b9de21230
= <=>
2021-04-19 23:17:24 +0700

    addToCart Sesion

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const cookieParser = require('cookie-parser')
 
 const userRoute=require('./routes/user.route')
 const authMiddleware=require('./middlewares/auth.middleware')
+const sessionMiddleware = require('./middlewares/session.middleware')
 const productRoute=require('./routes/product.route')
+const cartRoute=require('./routes/cart.route')
 const auth=require('./routes/auth.route')
 const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) 
 app.use(cookieParser(process.env.SESSION_SECRET))
+app.use(sessionMiddleware)
 
 app.set('view engine','pug')
 app.set('views','./views')
@@ -29,6 +32,7 @@ app.get('/', (req, res) => {
 app.use('/users',authMiddleware.requireAuth , userRoute)
 app.use('/auth',auth)
 app.use('/products', productRoute);
+app.use('/cart', cartRoute)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ middlewares/session.middleware.js
@@ -0,0 +1,40 @@
+
+// var shortid = require('shortid');
+
+// var db = require('../db');
+
+// module.exports = function(req, res, next) {
+//   if (!req.signedCookies.sessionId) {
+//     var sessionId = shortid.generate();
+//     res.cookie('sessionId', sessionId, {
+//       signed: true
+//     });
+
+//     db.get('sessions').push({
+//       id: sessionId
+//     }).write();
+//   }
+
+//   next();
+// }
+
+
+
+var shortid = require('shortid');
+
+var db = require('../db');
+
+module.exports = function(req, res, next) {
+  if (!req.signedCookies.sessionId) {
+    var sessionId = shortid.generate();
+    res.cookie('sessionId', sessionId, {
+      signed: true
+    });
+
+    db.get('sessions').push({
+      id: sessionId
+    }).write();
+  }
+
+  next();
+}
