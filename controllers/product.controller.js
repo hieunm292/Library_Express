383af0d9fe373a493367910f243fe23451cbbd6b
= <=>
2021-04-20 17:45:07 +0700

    mm

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,17 +1,24 @@
-const db=require('../db')
+const product=require('../models/product.model')
 
 module.exports.index=(req,res)=>{
 
-	var page=parseInt(req.query.page) || 1;  //n
-	var perPage=8;  //x
-	var start=(page-1)*perPage;
-	var end=page * perPage;
+	// var page=parseInt(req.query.page) || 1;  //n
+	// var perPage=8;  //x
+	// var start=(page-1)*perPage;
+	// var end=page * perPage;
 
 	// var drop = (page - 1) * perPage;
 
-    res.render('products/index', {
-		products:db.get('products').value().slice(start,end)
+    // res.render('products/index', {
+	// 	 products:db.get('products').value().slice(start,end)
 
-		 //   products: db.get('products').drop(drop).take(perPage).value()
-	});
+	// 	 products: db.get('products').drop(drop).take(perPage).value()
+	// });
+
+	product.find()
+	       .then((products)=>{ 
+			   res.render('products/index',{
+			   products
+			   })
+    })
 }
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
 const csurf = require('csurf')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URL)
 
 
 const userRoute=require('./routes/user.route')
